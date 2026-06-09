ab9e49c938b338e954cdb140977ea23391049c44
Qoqnus master <mahisokhary@gmail.com>
2026-06-09 20:48:01 +0330

    test: splited tests into multiple files

--- test/test.c
+++ test/test.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <qson/qson.h>
-#include <qson/_qson.h>
-
-#define test_run_log(m) printf("[*] Running test %s: ", m)
-#define test_result_log(s) printf("%s\n", s ? "passed" : "failed")
+#include "test.h"
 
 bool test_qson_skip_white_spaces() {
 	test_run_log("qson_skip_white_spaces");
@@ -31,44 +25,19 @@ bool test_qson_skip_white_spacesـunexpected_eof() {
 	return success;
 }
 
-bool test_qson_start_object() {
-	test_run_log("qson_start_object");
-	char buffer[] = "{";
-	qson_deserialize_ctx_t ctx;
-
-	bool success = 1;
-	success &= qson_create_deserialize_ctx(&ctx, buffer, array_len(buffer)) == QSON_RESULT_OK;
-	success &= qson_start_object(&ctx) == QSON_RESULT_OK;
-	success &= ctx.state == QSON_DESERIALIZING_STATE_OBJECT;
-	test_result_log(success);
-	return success;
-}
-
-bool test_qson_get_object_entry() {
-	test_run_log("qson_get_object_entry");
-	char buffer[] = "{ \"key\\u2764\" : \"string value\"  }";
-	qson_deserialize_ctx_t ctx;
-	char key[10];
-	int key_size = 10;
-	qson_type value_type = QSON_TYPE_AUTO;
-
-	bool success = 1;
-	success &= qson_create_deserialize_ctx(&ctx, buffer, array_len(buffer)) == QSON_RESULT_OK;
-	success &= qson_start_object(&ctx) == QSON_RESULT_OK;
-	success &= qson_get_object_entry(&ctx, key, &key_size, &value_type) == QSON_RESULT_OK;
-	success &= key_size == 7;
-	success &= strcmp(key, "key\u2764") == 0;
-	success &= value_type == QSON_TYPE_STRING;
-	test_result_log(success);
-	return success;
-}
-
-int main() {
+bool test() {
 	bool success = 1;
 	success &= test_qson_skip_white_spaces();
 	success &= test_qson_skip_white_spacesـunexpected_eof();
-	success &= test_qson_start_object();
-	success &= test_qson_get_object_entry();
+	return success;
+}
+
+extern bool test_object();
+
+int main() {
+	bool success = 1;
+	success &= test();
+	success &= test_object();
 	return !success;
 }
 
