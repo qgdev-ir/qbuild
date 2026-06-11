6be11613d770a2b9cc12d0a70db6e6b87ce5a04e
Qoqnus master <mahisokhary@gmail.com>
2026-06-11 11:25:46 +0330

    qson: Fixed "qson_get_object_entry ignores _qson_skip_white_spaces result"

--- qson/object.c
+++ qson/object.c
@@ -33,16 +33,23 @@ qson_result qson_start_object(qson_deserialize_ctx_t *ctx) {
 
 qson_result qson_get_object_entry(qson_deserialize_ctx_t *ctx, char *key, int *key_length, qson_type *type) {
 	if (ctx->state != QSON_DESERIALIZING_STATE_OBJECT) return QSON_RESULT_INVALID_STATE;
-	_qson_skip_white_spaces(ctx);
+
+	qson_result res = _qson_skip_white_spaces(ctx);
+	if (res != QSON_RESULT_OK) return res;
+
 	if (ctx->buffer[ctx->index] != QSON_QUOTATION_MARK) return QSON_RESULT_INVALID_CHAR;
 	ctx->state = QSON_DESERIALIZING_STATE_OBJECT_VALUE;
 
-	qson_result res = qson_read_string(ctx, key, key_length);
+	res = qson_read_string(ctx, key, key_length);
 	if (res != QSON_RESULT_OK) return res;
 
-	_qson_skip_white_spaces(ctx);
+	res = _qson_skip_white_spaces(ctx);
+	if (res != QSON_RESULT_OK) return res;
+
 	if (ctx->buffer[ctx->index++] != QSON_NAME_SEPARATOR) return QSON_RESULT_INVALID_CHAR;
-	_qson_skip_white_spaces(ctx);
+
+	res = _qson_skip_white_spaces(ctx);
+	if (res != QSON_RESULT_OK) return res;
 
 	if (*type == QSON_TYPE_AUTO) {
 		char chr = ctx->buffer[ctx->index];
