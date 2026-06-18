fab61c7980dce7b4a6994a544068f8cdb784b568
Qoqnus master <mahisokhary@gmail.com>
2026-06-18 11:40:04 +0330

    qson: Added enum qson_serialize_state

--- qson/serialize/serialize.h
+++ qson/serialize/serialize.h
@@ -5,6 +5,12 @@
 extern "C" {
 #endif
 
+/*
+ * Current state of serialization
+ */
+typedef enum {
+	QSON_SERIALIZE_STATE_NONE = 0,		// no specific serialization is running
+} qson_serialize_state;
 
 
 #ifdef __cplusplus
