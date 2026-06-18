30cedddfc54f936cbf77dbdf5736f906f5398abb
Qoqnus master <mahisokhary@gmail.com>
2026-06-18 11:46:45 +0330

    qson: Added header serialize/_serialize.h for internal use only

---
+++ qson/serialize/_serialize.h
@@ -0,0 +1,19 @@
+/*
+ * Its not part of qson API its for internal use only
+ */
+
+#ifndef _qson_serialize__serialize_h_
+#define _qson_serialize__serialize_h_
+
+#include <qson/_qson.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
