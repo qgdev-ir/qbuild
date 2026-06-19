f9211a3039cf20b3febedfe5faa783d90dee6a5d
Qoqnus master <mahisokhary@gmail.com>
2026-06-19 16:01:12 +0330

    qson: Added some helper static inline functions in serialize/object.c to help serializing object entries

--- qson/serialize/object.c
+++ qson/serialize/object.c
@@ -1,6 +1,22 @@
 #include <qson/qson.h>
 #include "_serialize.h"
 
+static inline qson_result _write_object_entry(qson_serialize_ctx_t *ctx, char *key) {
+	qson_run(qson_write_string(ctx, key));
+	qson_ctx_write(ctx, QSON_NAME_SEPARATOR);
+	return QSON_RESULT_OK;
+}
+
+static inline qson_result _handle_has_next(qson_serialize_ctx_t *ctx, bool has_next) {
+	if (has_next) {
+		qson_ctx_write(ctx, QSON_VALUE_SEPARATOR);
+	} else {
+		qson_ctx_write(ctx, QSON_END_OBJECT);
+		ctx->state = QSON_SERIALIZE_STATE_NONE;
+	}
+	return QSON_RESULT_OK;
+}
+
 qson_result qson_write_object(qson_serialize_ctx_t *ctx) {
 	if (ctx->state != QSON_SERIALIZE_STATE_NONE) return QSON_RESULT_INVALID_STATE;
 	qson_ctx_write(ctx, QSON_BEGIN_OBJECT);
