825d10f6d0e92171b4d1514bc8ee24fd923f6536
Qoqnus master <mahisokhary@gmail.com>
2026-06-19 15:57:20 +0330

    qson: Added qson_write_string function

--- qson/serialize/serialize.c
+++ qson/serialize/serialize.c
@@ -15,3 +15,35 @@ qson_result qson_end_serialize_ctx(qson_serialize_ctx_t *ctx) {
 	return QSON_RESULT_OK;
 }
 
+static inline qson_result _qson_serialize_string_write_escaped(qson_serialize_ctx_t *ctx, char chr) {
+	qson_ctx_write(ctx, QSON_STRING_ESCAPE_CHAR);
+	qson_ctx_write(ctx, chr);
+	return QSON_RESULT_OK;
+}
+
+qson_result qson_write_string(qson_serialize_ctx_t *ctx, char *value) {
+	qson_ctx_write(ctx, QSON_QUOTATION_MARK);
+
+	int i = 0;
+	char chr;
+	while ((chr = value[i]) != '\0') {
+		switch (chr) {
+		case '\b':
+		case '\f':
+		case '\n':
+		case '\r':
+		case '\t':
+		case QSON_QUOTATION_MARK:
+			qson_run(_qson_serialize_string_write_escaped(ctx, chr));
+			break;
+		default:
+			if (chr < 32 || 126 < chr) return QSON_RESULT_INVALID_CHAR;
+			qson_ctx_write(ctx, chr);
+			break;
+		}
+		i++;
+	}
+	qson_ctx_write(ctx, QSON_QUOTATION_MARK);
+	return QSON_RESULT_OK;
+}
+
