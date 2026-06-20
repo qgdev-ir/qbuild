d20fd3d0821e9029d9831f76336cfa6251928861
Qoqnus master <mahisokhary@gmail.com>
2026-06-20 10:10:51 +0330

    qson: Fixed "qson_write_object function dont set state and qson_write_object_entry_string dont check state" (#45)

--- qson/serialize/object.c
+++ qson/serialize/object.c
@@ -20,10 +20,12 @@ static inline qson_result _handle_has_next(qson_serialize_ctx_t *ctx, bool has_n
 qson_result qson_write_object(qson_serialize_ctx_t *ctx) {
 	if (ctx->state != QSON_SERIALIZE_STATE_NONE) return QSON_RESULT_INVALID_STATE;
 	qson_ctx_write(ctx, QSON_BEGIN_OBJECT);
+	ctx->state = QSON_SERIALIZE_STATE_OBJECT;
 	return QSON_RESULT_OK;
 }
 
 qson_result qson_write_object_entry_string(qson_serialize_ctx_t *ctx, char *key, char *value, bool has_next) {
+	if (ctx->state != QSON_SERIALIZE_STATE_OBJECT) return QSON_RESULT_INVALID_STATE;
 	qson_run(_write_object_entry(ctx, key));
 	qson_run(qson_write_string(ctx, value));
 	qson_run(_handle_has_next(ctx, has_next));
