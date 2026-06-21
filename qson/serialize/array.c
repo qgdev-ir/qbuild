c6c91867a34e6739228d65d3530b7682c7f559bf
Qoqnus master <mahisokhary@gmail.com>
2026-06-21 14:22:07 +0330

    qson: Added inline static helper function _handle_has_next in qson/serialize/array.c

--- qson/serialize/array.c
+++ qson/serialize/array.c
@@ -1,6 +1,16 @@
 #include <qson/qson.h>
 #include "_serialize.h"
 
+static inline qson_result _handle_has_next(qson_serialize_ctx_t *ctx, bool has_next) {
+	if (has_next) {
+		qson_ctx_write(ctx, QSON_VALUE_SEPARATOR);
+	} else {
+		qson_ctx_write(ctx, QSON_END_ARRAY);
+		ctx->state = QSON_SERIALIZE_STATE_NONE;
+	}
+	return QSON_RESULT_OK;
+}
+
 qson_result qson_write_array(qson_serialize_ctx_t *ctx) {
 	if (ctx->state != QSON_SERIALIZE_STATE_NONE) return QSON_RESULT_INVALID_STATE;
 	qson_ctx_write(ctx, QSON_BEGIN_ARRAY);
