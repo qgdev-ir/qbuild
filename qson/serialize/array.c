7bcc856bcccefa9c99d0ba31561a579d686af516
Qoqnus master <mahisokhary@gmail.com>
2026-06-21 11:10:46 +0330

    qson: Added function qson_write_array

--- qson/serialize/array.c
+++ qson/serialize/array.c
@@ -1,4 +1,10 @@
 #include <qson/qson.h>
 #include "_serialize.h"
 
+qson_result qson_write_array(qson_serialize_ctx_t *ctx) {
+	if (ctx->state != QSON_SERIALIZE_STATE_NONE) return QSON_RESULT_INVALID_STATE;
+	qson_ctx_write(ctx, QSON_BEGIN_ARRAY);
+	ctx->state = QSON_SERIALIZE_STATE_ARRAY;
+	return QSON_RESULT_OK;
+}
 
--- qson/serialize/array.h
+++ qson/serialize/array.h
@@ -4,6 +4,11 @@
 extern "C" {
 #endif
 
+/*
+ * Start writing an array and set context state to ARRAY
+ * Requires state NONE
+ */
+qson_result qson_write_array(qson_serialize_ctx_t *ctx);
 
 #ifdef __cplusplus
 }
