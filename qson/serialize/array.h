7671f1575018ed74e6519544c98fcdd5111eb89a
Qoqnus master <mahisokhary@gmail.com>
2026-06-23 14:38:23 +0330

    qson: Fixed comment of qson_write_array_entry_bool

--- qson/serialize/array.h
+++ qson/serialize/array.h
@@ -29,7 +29,7 @@ qson_result qson_write_array_entry_null(qson_serialize_ctx_t *ctx, bool has_next
 /*
  * Add an array entry with value type of boolean
  * if "has_next" writes chars needed for next entry
- * and if not writes chars needed for object end and sets state to NONE
+ * and if not writes chars needed for array end and sets state to NONE
  * Requires state ARRAY
  */
 qson_result qson_write_array_entry_bool(qson_serialize_ctx_t *ctx, bool value, bool has_next);
