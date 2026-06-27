2a0f8fec1cc5bc96c7ee31bb331efd2323578149
Qoqnus master <mahisokhary@gmail.com>
2026-06-27 12:12:31 +0330

    qstruct: Included headers from stdc that will be needed by module and created basic typedefs

--- qstruct/qstruct.h
+++ qstruct/qstruct.h
@@ -1,8 +1,19 @@
 #ifndef _qstruct_qstruct_h_
 #define _qstruct_qstruct_h_
+
+#include <stddef.h>
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/*
+ * Result of qstruct functions
+ */
+typedef enum {
+	QSTRUCT_RESULT_OK = 0,		// successful
+} qstruct_result_t;
 
 #ifdef __cplusplus
 }
