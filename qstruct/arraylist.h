93145da812a09861b425ca9771cfa1f34315dff0
Qoqnus master <mahisokhary@gmail.com>
2026-06-27 16:49:18 +0330

    qstruct/arraylist: Added qstruct_arraylist_create function

--- qstruct/arraylist.h
+++ qstruct/arraylist.h
@@ -11,6 +11,13 @@ extern "C" {
  */
 typedef uint64_t qstruct_arraylist_t;
 
+/*
+ * Creates an arraylist
+ * value_size: size of each value in array
+ * initialize_capacity: initial size of array (if 0 value of QSTRUCT_ARRAYLIST_DEFAULT_INITIALIZE_CAPACITY is used instead)
+ */
+qstruct_result_t qstruct_arraylist_create(qstruct_arraylist_t *arraylist, size_t value_size, size_t initialize_capacity);
+
 #ifdef __cplusplus
 }
 #endif
