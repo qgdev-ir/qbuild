83c8d8575032f5639ada756208556beb70595345
Qoqnus master <mahisokhary@gmail.com>
2026-06-28 14:45:59 +0330

    test: Added test group qstruct/arraylist

---
+++ test/qstruct/arraylist.c
@@ -0,0 +1,6 @@
+#include <test/test.h>
+
+bool test_qstruct_arraylist() {
+	bool success = 1;
+	return success;
+}
--- test/qstruct/qstruct.c
+++ test/qstruct/qstruct.c
@@ -1,6 +1,9 @@
 #include <test/test.h>
 
+extern bool test_qstruct_arraylist();
+
 bool test_qstruct() {
 	bool success = 1;
+	success &= test_qstruct_arraylist();
 	return success;
 }
