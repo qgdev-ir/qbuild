a8cc32c35a57995ae1e3413209c9b8a07b52c5b3
Qoqnus master <mahisokhary@gmail.com>
2026-06-30 10:42:04 +0330

    qstruct/linkedlist: Created private structs for linkedlist and its entries

--- qstruct/linkedlist.c
+++ qstruct/linkedlist.c
@@ -1,2 +1,16 @@
 #include <qstruct/qstruct.h>
 
+struct linkedlist {
+	size_t length;		// length size
+	size_t value_size;	// size of each value
+	struct entry *entry;	// pointer to first entry
+};
+
+struct entry {
+	struct entry *next;	// Next entry
+	/*
+	 * Value will be placed after this struct
+	 * And value size is the value_size in linkedlist struct
+	 */
+};
+
