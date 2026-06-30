bc6537b15526f6ef3c044d6d30232da7bd651ce1
Qoqnus master <mahisokhary@gmail.com>
2026-06-30 14:55:30 +0330

    qstruct/linkedlist: Stored previous entry in entries and last entry in linked list: this will improve performance in some operations (GitHub #114)
    qstruct/linkedlist:
    - Stored previous entry in entries and last entry in linked list: this will improve performance in some operations
    - Updated function qstruct_linkedlist_create to set entry and lentry fields of linkedlist struct with NULL

--- qstruct/linkedlist.c
+++ qstruct/linkedlist.c
@@ -5,9 +5,11 @@ struct linkedlist {
 	size_t length;		// length size
 	size_t value_size;	// size of each value
 	struct entry *entry;	// pointer to first entry
+	struct entry *lentry;	// pointer to last entry
 };
 
 struct entry {
+	struct entry *previous;	// Previous entry
 	struct entry *next;	// Next entry
 	/*
 	 * Value will be placed after this struct
@@ -19,6 +21,8 @@ qstruct_result_t qstruct_linkedlist_create(qstruct_linkedlist_t *list, size_t va
 	struct linkedlist *ll = malloc(sizeof(struct linkedlist));
 	ll->length = 0;
 	ll->value_size = value_size;
+	ll->entry = NULL;
+	ll->lentry = NULL;
 	*list = ll;
 	return QSTRUCT_RESULT_OK;
 }
