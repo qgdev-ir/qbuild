1f705e13a01f3eab6a49b13281dca59a429a7c54
Qoqnus master <mahisokhary@gmail.com>
2026-07-01 08:33:44 +0330

    test/qstruct/linkedlist: Added test for function qstruct_linkedlist_remove

--- test/qstruct/linkedlist.c
+++ test/qstruct/linkedlist.c
@@ -39,10 +39,42 @@ bool test_qstruct_linkedlist_length() {
 	return success;
 }
 
+bool test_qstruct_linkedlist_remove() {
+	test_run_log("qstruct_linkedlist_remove");
+
+	qstruct_linkedlist_t list;
+	int value = 858585;
+	int value2 = 696969;
+	int value3 = 262626;
+	int res_value;
+
+	bool success = 1;
+	success &= qstruct_linkedlist_create(&list, sizeof(int)) == QSTRUCT_RESULT_OK;
+	success &= qstruct_linkedlist_add(list, &value) == QSTRUCT_RESULT_OK;
+	success &= qstruct_linkedlist_add(list, &value2) == QSTRUCT_RESULT_OK;
+	success &= qstruct_linkedlist_add(list, &value3) == QSTRUCT_RESULT_OK;
+
+	success &= qstruct_linkedlist_remove(list, 1) == QSTRUCT_RESULT_OK;
+	success &= qstruct_linkedlist_get(list, 1, &res_value) == QSTRUCT_RESULT_OK;
+	success &= res_value == value3;
+
+	success &= qstruct_linkedlist_remove(list, 0) == QSTRUCT_RESULT_OK;
+	success &= qstruct_linkedlist_get(list, 0, &res_value) == QSTRUCT_RESULT_OK;
+	success &= res_value == value3;
+
+	success &= qstruct_linkedlist_remove(list, 0) == QSTRUCT_RESULT_OK;
+	success &= qstruct_linkedlist_get(list, 0, &res_value) == QSTRUCT_RESULT_INDEX_OUTOF_BOUND;
+
+	success &= qstruct_linkedlist_destroy(list) == QSTRUCT_RESULT_OK;
+	test_result_log(success);
+	return success;
+}
+
 bool test_qstruct_linkedlist() {
 	bool success = 1;
 	success &= test_qstruct_linkedlist_add_get();
 	success &= test_qstruct_linkedlist_length();
+	success &= test_qstruct_linkedlist_remove();
 	return success;
 }
 
