0b6bdb702f34563ef9a467bd20a08ce1db5bf230
Qoqnus master <mahisokhary@gmail.com>
2026-07-01 09:03:34 +0330

    qstruct: Added internal header _qstruct

---
+++ qstruct/_qstruct.h
@@ -0,0 +1,17 @@
+/*
+ * This header file is NOT part of the qstruct API
+ * FOR INTERNAL USE ONLY
+ */
+#ifndef _qstruct__qstruct_h_
+#define _qstruct__qstruct_h_
+
+#include <qstruct/qstruct.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
