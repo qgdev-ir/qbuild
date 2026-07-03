5a976d9f98436f894b6f508b8281b00e1f9b1c93
Qoqnus master <mahisokhary@gmail.com>
2026-07-03 12:39:57 +0330

    qstruct/rbtree: Added structs needed for rbtree

--- qstruct/rbtree.c
+++ qstruct/rbtree.c
@@ -1,2 +1,15 @@
 #include <qstruct/_qstruct.h>
 
+struct rbtree {
+	qstruct_rbtree_comparator_t comparator;
+	struct node *root;
+};
+
+struct node {
+	struct node *parent;
+	struct node *right;
+	struct node *left;
+	bool red;
+	char value[];
+};
+
