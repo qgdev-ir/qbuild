7599497b2aa67819681aebd1b22f3b994aa48e02
Qoqnus master <mahisokhary@gmail.com>
2026-07-03 12:30:17 +0330

    qstruct/rbtree: Added typedef qstruct_rbtree_comparator_t which is a function to compare two values

--- qstruct/rbtree.h
+++ qstruct/rbtree.h
@@ -9,6 +9,14 @@ extern "C" {
  */
 typedef void* qstruct_rbtree_t;
 
+/*
+ * Compares to values and returns a number as result:
+ * -1: x is smaller than y
+ * 0: values are equal
+ * 1: x is bigger than y
+ */
+typedef int8_t (*qstruct_rbtree_comparator_t)(char *x, char *y);
+
 #ifdef __cplusplus
 }
 #endif
