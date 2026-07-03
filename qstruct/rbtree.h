1205e1c602b9fd7547760dba53b4b251cad016f1
Qoqnus master <mahisokhary@gmail.com>
2026-07-03 08:30:49 +0330

    qstruct: Created headers, sources and typdefs for red-black tree (GitHub #141)
    qstruct/rbtree:
    - Added header rbtree and included it in header qstruct
    - Added source rbtree to implement header rbtree
    - Added typedef qstruct_rbtree_t

---
+++ qstruct/rbtree.h
@@ -0,0 +1,15 @@
+#ifndef _qstruct_rbtree_h_
+#define _qstruct_rbtree_h_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Pointer to actual red-black tree
+ */
+typedef void* qstruct_rbtree_t;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
