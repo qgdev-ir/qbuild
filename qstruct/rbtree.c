9377189f74d554d2f06d6fffdd01f4c23d160eda
Qoqnus master <mahisokhary@gmail.com>
2026-07-04 10:17:00 +0330

    qstruct/rbtree: Added functions and structs to create a rbtree (GitHub #147)
    qstruct/rbtree:
    - Added typedef qstruct_rbtree_comparator_t which is a function to compare two buffers
    - Added structs needed for rbtree
    - Added function qstruct_rbtree_create
    - Included stdbool in rbtree source

--- qstruct/rbtree.c
+++ qstruct/rbtree.c
@@ -1,2 +1,23 @@
 #include <qstruct/_qstruct.h>
+#include <stdbool.h>
+
+struct rbtree {
+	qstruct_rbtree_comparator_t comparator;
+	struct node *root;
+};
+
+struct node {
+	struct node *parent;
+	struct node *right;
+	struct node *left;
+	bool red;
+	char value[];
+};
+
+qstruct_result_t qstruct_rbtree_create(qstruct_rbtree_t *tree, qstruct_rbtree_comparator_t comparator) {
+	struct rbtree *t = malloc(sizeof(struct rbtree));
+	t->comparator = comparator;
+	t->root = NULL;
+	return QSTRUCT_RESULT_OK;
+}
 
--- qstruct/rbtree.h
+++ qstruct/rbtree.h
@@ -9,6 +9,19 @@ extern "C" {
  */
 typedef void* qstruct_rbtree_t;
 
+/*
+ * Compares to values and returns a number as result:
+ * -1: x is smaller than y
+ * 0: values are equal
+ * 1: x is bigger than y
+ */
+typedef int8_t (*qstruct_rbtree_comparator_t)(char *x, char *y);
+
+/*
+ * Creates a red-black tree
+ */
+qstruct_result_t qstruct_rbtree_create(qstruct_rbtree_t *tree, qstruct_rbtree_comparator_t comparator);
+
 #ifdef __cplusplus
 }
 #endif
