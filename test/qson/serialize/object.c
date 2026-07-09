d1538296ef08b37ed590af86e47d9c9360e3957d
Qoqnus master <mahisokhary@gmail.com>
2026-07-09 09:49:12 +0330

    test/qson/serialize/object: Updated test qson_write_object to use function qson_serialize_object_end

--- test/qson/serialize/object.c
+++ test/qson/serialize/object.c
@@ -3,15 +3,15 @@
 
 bool test_qson_write_object() {
 	test_run_log("qson_write_object");
-	char buffer[2];
+	char buffer[3];
 	qson_serialize_ctx_t ctx;
 
 	bool success = 1;
 	success &= qson_create_serialize_ctx(&ctx, buffer, array_len(buffer)) == QSON_RESULT_OK;
 	success &= qson_write_object(&ctx) == QSON_RESULT_OK;
-	ctx.state = QSON_SERIALIZE_STATE_NONE; // Faking none state
+	success &= qson_serialize_object_end(&ctx) == QSON_RESULT_OK;
 	success &= qson_end_serialize_ctx(&ctx) == QSON_RESULT_OK;
-	success &= strcmp(buffer, "{\0") == 0;
+	success &= strcmp(buffer, "{}\0") == 0;
 	test_result_log(success);
 	return success;
 }
