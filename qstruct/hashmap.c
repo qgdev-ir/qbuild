aa3f48b3b2d83110f48b42daa41fda3cf8ce328e
Qoqnus master <mahisokhary@gmail.com>
2026-07-14 17:24:31 +0330

    qstruct/hashmap: Removed field value_size from internal struct hashmap
    this help hashmap to contains values with diffrent sizes

--- qstruct/hashmap.c
+++ qstruct/hashmap.c
@@ -1,7 +1,6 @@
 #include <qstruct/_qstruct.h>
 
 struct hashmap {
-	size_t value_size;		// Size of each value
 	size_t length;			// How much is current array filled
 	qstruct_rbtree_t *buckets;	// Buckets
 	size_t capacity;		// Capacity of buckets
