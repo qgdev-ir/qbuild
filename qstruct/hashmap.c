d391746cb21e9afb85e6c24d9920b97636bb3b24
Qoqnus master <mahisokhary@gmail.com>
2026-07-16 10:01:35 +0330

    qstruct/hashmap: Added new fields to internal struct hashmap

--- qstruct/hashmap.c
+++ qstruct/hashmap.c
@@ -4,5 +4,9 @@ struct hashmap {
 	size_t length;			// How much is current array filled
 	qstruct_rbtree_t *buckets;	// Buckets
 	size_t capacity;		// Capacity of buckets
+	qstruct_rbtree_comparator_t comparator;	// Used for handling
+	double max_loadfactor;			// max load factor
+	qstruct_hashmap_hasher_t hasher;	// Hash function
+	long seed;				// Seed for hash function
 };
 
