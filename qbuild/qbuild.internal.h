2ee4ae047e9c8c98decddcb50d8ef6917188eb24
Qoqnus master <mahisokhary@gmail.com>
2026-07-22 09:27:54 +0330

    qbuild: Added internal header qbuild

---
+++ qbuild/qbuild.internal.h
@@ -0,0 +1,15 @@
+#ifndef _qson_qson_internal_h_
+#define _qson_qson_internal_h_
+
+#include <qbuild/qbuild.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
