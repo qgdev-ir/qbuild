7c61641bc4f0ff5e79d95f48b2f8946dc0b77960
Qoqnus master <mahisokhary@gmail.com>
2026-07-24 13:34:50 +0330

    qbuild/file: Added internal header file

---
+++ qbuild/file/file.internal.h
@@ -0,0 +1,20 @@
+/*
+ * QBuild file helper
+ * Built to help reading and writing to files
+ */
+
+#ifndef _qbuild_file_file_internal_h_
+#define _qbuild_file_file_internal_h_
+
+#include <qbuild/qbuild.internal.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
