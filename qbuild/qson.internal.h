3b4734fe6e91b15fce890c7a905ece85ddbe6781
Qoqnus master <mahisokhary@gmail.com>
2026-07-24 10:49:30 +0330

    qbuild: Created internal header qson

---
+++ qbuild/qson.internal.h
@@ -0,0 +1,19 @@
+/*
+ * QBuild internal helper for qson
+ */
+
+#ifndef _qbuild_qson_internal_h_
+#define _qbuild_qson_internal_h_
+
+#include <qson/qson.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
