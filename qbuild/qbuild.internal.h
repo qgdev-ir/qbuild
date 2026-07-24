403bfbc13080da4f19878ee15cdd0f737fe55468
Qoqnus master <mahisokhary@gmail.com>
2026-07-24 14:14:46 +0330

    qbuild: Included headers needed by module

--- qbuild/qbuild.internal.h
+++ qbuild/qbuild.internal.h
@@ -1,6 +1,9 @@
 #ifndef _qbuild_qbuild_internal_h_
 #define _qbuild_qbuild_internal_h_
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <qbuild/qbuild.h>
 #include <qbuild/file/file.internal.h>
 
