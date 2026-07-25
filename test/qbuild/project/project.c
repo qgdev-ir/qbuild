2ff9baf879cef8f2bd4595c435311a18df75fe0d
Qoqnus master <mahisokhary@gmail.com>
2026-07-25 13:54:15 +0330

    test/qbuild/project: Added test for function qbuild_project_load

--- test/qbuild/project/project.c
+++ test/qbuild/project/project.c
@@ -1,7 +1,23 @@
 #include <test/test.h>
+#include <qbuild/qbuild.h>
+#include <string.h>
+
+bool test_qbuild_project_load() {
+	test_run_log("qbuild_project_load");
+	qbuild_project_t p;
+
+	bool success = true;
+	success &= qbuild_project_load(&p, "example") == QBUILD_RESULT_OK;
+	success &= strcmp(qbuild_project_name(p), "qbuild-example") == 0;
+	success &= strcmp(qbuild_project_version(p), "1.0.0-beta") == 0;
+	success &= qbuild_project_destroy(p) == QBUILD_RESULT_OK;
+	test_result_log(success);
+	return success;
+}
 
 bool test_qbuild_project() {
 	bool success = true;
+	success &= test_qbuild_project_load();
 	return success;
 }
 
