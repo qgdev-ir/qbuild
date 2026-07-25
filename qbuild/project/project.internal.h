0367c5dd374cd5c36a5152efa45cc03d0679abb9
Qoqnus master <mahisokhary@gmail.com>
2026-07-25 09:19:04 +0330

    qbuild/project: Created internal header project

---
+++ qbuild/project/project.internal.h
@@ -0,0 +1,16 @@
+#ifndef _qbuild_project_project_internal_h_
+#define _qbuild_project_project_internal_h_
+
+#include <qbuild/qbuild.internal.h>
+#include <qbuild/project/project.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
