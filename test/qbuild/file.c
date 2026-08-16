a6205a1ed2d9357b9645d13d7fc138dd0783802b
Qoqnus master <mahisokhary@gmail.com>
2026-08-16 14:22:50 +0330

    test/qbuild: Created test group file

---
+++ test/qbuild/file.c
@@ -0,0 +1,8 @@
+#include <test/test.h>
+#include <qbuild/file/file.internal.h>
+
+bool test_qbuild_file() {
+	bool success = true;
+	return success;
+}
+
--- test/qbuild/qbuild.c
+++ test/qbuild/qbuild.c
@@ -1,10 +1,12 @@
 #include <test/test.h>
 
 extern bool test_qbuild_project();
+extern bool test_qbuild_file();
 
 bool test_qbuild() {
 	bool success = true;
 	success &= test_qbuild_project();
+	success &= test_qbuild_file();
 	return success;
 }
 
