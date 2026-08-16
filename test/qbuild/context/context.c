cb0b6f4ce596314d8a093d577e92c8469b310e96
Qoqnus master <mahisokhary@gmail.com>
2026-08-16 16:45:15 +0330

    test/qbuild: Created test group for context

---
+++ test/qbuild/context/context.c
@@ -0,0 +1,8 @@
+#include <test/test.h>
+#include <qbuild/qbuild.internal.h>
+
+bool test_qbuild_context() {
+	bool success = true;
+	return success;
+}
+
--- test/qbuild/qbuild.c
+++ test/qbuild/qbuild.c
@@ -2,11 +2,13 @@
 
 extern bool test_qbuild_project();
 extern bool test_qbuild_file();
+extern bool test_qbuild_context();
 
 bool test_qbuild() {
 	bool success = true;
 	success &= test_qbuild_project();
 	success &= test_qbuild_file();
+	success &= test_qbuild_context();
 	return success;
 }
 
