3f036c03f171dbc45de939cc9bdc7bdb78d30a00
Qoqnus master <mahisokhary@gmail.com>
2026-08-16 17:06:00 +0330

    qbuild/context: Added function qbuild_context_create

--- qbuild/context/context.c
+++ qbuild/context/context.c
@@ -1,2 +1,14 @@
 #include "context.internal.h"
 
+qbuild_result_t qbuild_context_create(qbuild_context_t *ctx) {
+	qbuild_context_t c = malloc(sizeof (struct qbuild_context));
+
+	const char *config_dirs = getenv("QBUILD_CONTEXT_CONFIG_PATH");
+	if (!config_dirs) config_dirs = QBUILD_CONTEXT_CONFIG_PATH;
+	c->config_dirs = qbuild_file_path_separate(config_dirs, &c->config_dirs_length);
+	if (!c->config_dirs) return QBUILD_RESULT_CONTEXT_CREATE_FAILED;
+
+	*ctx = c;
+	return QBUILD_RESULT_OK;
+}
+
--- qbuild/context/context.h
+++ qbuild/context/context.h
@@ -10,6 +10,11 @@ extern "C" {
  */
 typedef struct qbuild_context* qbuild_context_t;
 
+/*
+ * Create a qbuild context
+ */
+qbuild_result_t qbuild_context_create(qbuild_context_t *ctx);
+
 #ifdef __cplusplus
 }
 #endif
