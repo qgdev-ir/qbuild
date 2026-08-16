0a960efa29f7556b064011620b430ae41288c3b7
Qoqnus master <mahisokhary@gmail.com>
2026-08-16 17:06:42 +0330

    test/qbuild/context: Added test for functions qbuild_context_create and qbuild_context_destroy

--- test/qbuild/context/context.c
+++ test/qbuild/context/context.c
@@ -1,8 +1,20 @@
 #include <test/test.h>
 #include <qbuild/qbuild.internal.h>
 
+bool test_qbuild_context_create_destroy() {
+	test_run_log("qbuild_context_create_destroy");
+	qbuild_context_t ctx;
+
+	bool success = 1;
+	success &= qbuild_context_create(&ctx) == QBUILD_RESULT_OK;
+	success &= qbuild_context_destroy(ctx) == QBUILD_RESULT_OK;
+	test_result_log(success);
+	return success;
+}
+
 bool test_qbuild_context() {
 	bool success = true;
+	success &= test_qbuild_context_create_destroy();
 	return success;
 }
 
