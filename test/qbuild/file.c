f38f14b8bf4f58795ced474726603e96a4f10db8
Qoqnus master <mahisokhary@gmail.com>
2026-08-16 14:28:37 +0330

    test/qbuild/file: Added test for function qbuild_file_path_separate

--- test/qbuild/file.c
+++ test/qbuild/file.c
@@ -1,8 +1,27 @@
 #include <test/test.h>
 #include <qbuild/file/file.internal.h>
 
+bool test_qbuild_file_path_seperate() {
+	test_run_log("qbuild_file_path_seperate");
+	const char sample[] = "test1.txt:test2.txt:test3.txt:test4.txt:test5.txt";
+	size_t size;
+	char **res;
+
+	bool success = 1;
+	success &= (res = qbuild_file_path_separate(sample, &size)) != NULL;
+	success &= size == 5;
+	for (int i = 0; i < size; i++) {
+		success &= memcmp(res[i], &sample[i * 10], 9) == 0;
+	}
+	free(res[0]);
+	free(res);
+	test_result_log(success);
+	return success;
+}
+
 bool test_qbuild_file() {
 	bool success = true;
+	success &= test_qbuild_file_path_seperate();
 	return success;
 }
 
