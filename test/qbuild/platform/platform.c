dd4883d4e56e08afa3bf5b830fb7dd17b7ec66b3
Qoqnus master <mahisokhary@gmail.com>
2026-08-20 09:44:01 +0330

    test/qbuild: Created test group platform

---
+++ test/qbuild/platform/platform.c
@@ -0,0 +1,7 @@
+#include <test/test.h>
+
+bool test_qbuild_platform() {
+	bool success = true;
+	return success;
+}
+
--- test/qbuild/qbuild.c
+++ test/qbuild/qbuild.c
@@ -3,12 +3,14 @@
 extern bool test_qbuild_project();
 extern bool test_qbuild_file();
 extern bool test_qbuild_context();
+extern bool test_qbuild_platform();
 
 bool test_qbuild() {
 	bool success = true;
 	success &= test_qbuild_project();
 	success &= test_qbuild_file();
 	success &= test_qbuild_context();
+	success &= test_qbuild_platform();
 	return success;
 }
 
