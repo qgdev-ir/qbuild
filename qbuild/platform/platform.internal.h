593228e8e713ada1fba31c929eef9871751b5291
Qoqnus master <mahisokhary@gmail.com>
2026-08-20 09:27:48 +0330

    qbuild/platform: Created internal header platform

---
+++ qbuild/platform/platform.internal.h
@@ -0,0 +1,15 @@
+#ifndef _qbuild_platform_platform_internal_h_
+#define _qbuild_platform_platform_internal_h_
+
+#include <qbuild/qbuild.internal.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
