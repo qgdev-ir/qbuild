83508a4273e9225afd512fc44b240a5e9d6dc745
Qoqnus master <mahisokhary@gmail.com>
2026-08-23 13:26:44 +0330

    qbuild: Included all internal headers in internal header qbuild

--- qbuild/qbuild.internal.h
+++ qbuild/qbuild.internal.h
@@ -21,6 +21,9 @@ extern "C" {
 
 #include <qbuild/qson.internal.h>
 #include <qbuild/file/file.internal.h>
+#include <qbuild/context/context.internal.h>
+#include <qbuild/platform/platform.internal.h>
+#include <qbuild/project/project.internal.h>
 
 #endif
 
