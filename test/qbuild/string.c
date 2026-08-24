5bc9f16291c13c449f92c594ac849f6ee1f6e6d4
Qoqnus master <mahisokhary@gmail.com>
2026-08-24 13:40:22 +0330

    test/qbuild/string: Added test for function qbuild_string_lower

--- test/qbuild/string.c
+++ test/qbuild/string.c
@@ -1,8 +1,21 @@
 #include <test/test.h>
 #include <qbuild/qbuild.internal.h>
 
+bool test_qbuild_string_lower() {
+	test_run_log("qbuild_string_lower");
+	char upper[] = "ASDfdsg/%_+";
+	char lower[] = "asdfdsg/%_+";
+
+	bool success = true;
+	qbuild_string_lower(upper);
+	success &= strcmp(upper, lower) == 0;
+	test_result_log(success);
+	return success;
+}
+
 bool test_qbuild_string() {
 	bool success = true;
+	success &= test_qbuild_string_lower();
 	return success;
 }
 
