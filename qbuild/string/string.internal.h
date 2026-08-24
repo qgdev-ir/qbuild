cadddea8ca2b20dccb70a5c0862000b354ba26a9
Qoqnus master <mahisokhary@gmail.com>
2026-08-24 13:30:20 +0330

    qbuild/string: Created internal header string

---
+++ qbuild/string/string.internal.h
@@ -0,0 +1,18 @@
+/*
+ * QBuild string helper
+ * Built to help operations on strings
+ */
+
+#ifndef _qbuild_string_string_internal_h_
+#define _qbuild_string_string_internal_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
