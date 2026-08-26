7af43cc558426d56bcad040658bf19237ac1a906
Qoqnus master <mahisokhary@gmail.com>
2026-08-26 08:44:42 +0330

    qbuild/platform/detect: Added support for detecting POSIX host platforms to function qbuild_platform_host_name

--- qbuild/platform/detect.c
+++ qbuild/platform/detect.c
@@ -1,6 +1,16 @@
 #include <qbuild/qbuild.internal.h>
+#ifdef __POSIX__
+#include <sys/utsname.h>
+#endif
 
 char *qbuild_platform_host_name() {
+#ifdef __POSIX__
+	struct utsname s;
+	uname(&s);
+	qbuild_string_lower(s.sysname);
+	return strdup(s.sysname);
+#else
 	return NULL;
+#endif
 }
 
