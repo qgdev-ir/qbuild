d39e642937d35d7b0fe48a70bd9acb9ebd0c0aa6
Qoqnus master <mahisokhary@gmail.com>
2026-08-26 08:53:16 +0330

    test/qbuild/platform: Added test for function qbuild_platform_host_name

--- test/qbuild/platform/platform.c
+++ test/qbuild/platform/platform.c
@@ -1,7 +1,22 @@
 #include <test/test.h>
+#include <qbuild/qbuild.internal.h>
+
+bool test_qbuild_platform_host_name() {
+	test_run_log("qbuild_platform_host_name");
+	char *host = qbuild_platform_host_name();
+
+	bool success = true;
+#ifdef TEST_QBUILD_PLATFORM_HOST_NAME
+	success &= strcmp(host, TEST_QBUILD_PLATFORM_HOST_NAME) == 0;
+#endif
+	free(host);
+	test_result_log(success);
+	return success;
+}
 
 bool test_qbuild_platform() {
 	bool success = true;
+	success &= test_qbuild_platform_host_name();
 	return success;
 }
 
