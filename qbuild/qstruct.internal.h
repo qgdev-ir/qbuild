504485b968d30f0f1a651f0b66c0007c21b79adb
Qoqnus master <mahisokhary@gmail.com>
2026-08-27 14:04:11 +0330

    qbuild: Created internal header qstruct

---
+++ qbuild/qstruct.internal.h
@@ -0,0 +1,19 @@
+/*
+ * QBuild internal helper for qstruct
+ */
+
+#ifndef _qbuild_qstruct_internal_h_
+#define _qbuild_qstruct_internal_h_
+
+#include <qstruct/qstruct.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
