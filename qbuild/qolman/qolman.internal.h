d08fcabc4f7eb406fe7c5098c6b0a12c48aa9f1e
Qoqnus master <mahisokhary@gmail.com>
2026-09-19 21:18:17 +0330

    qbuild: Created internal package qolman (GitHub #362)
    qbuild/qolman:
    - Created internal header qolman
    - Included external header qolman/qolman in internal header qolman
    - Created source qolman
    
    qbuild:
    - Included internal header qolman/qolman in internal header qbuild

---
+++ qbuild/qolman/qolman.internal.h
@@ -0,0 +1,19 @@
+/*
+ * QBuild internal helper for qolman
+ */
+
+#ifndef _qbuild_qolman_internal_h_
+#define _qbuild_qolman_internal_h_
+
+#include <qolman/qolman.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
