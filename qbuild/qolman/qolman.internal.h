988188a69826a4472975721b53ad1c0da78fe10b
Qoqnus master <mahisokhary@gmail.com>
2026-09-19 21:13:29 +0330

    qbuild/qolman: Created internal header qolman

---
+++ qbuild/qolman/qolman.internal.h
@@ -0,0 +1,18 @@
+/*
+ * QBuild internal helper for qolman
+ */
+
+#ifndef _qbuild_qolman_internal_h_
+#define _qbuild_qolman_internal_h_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
