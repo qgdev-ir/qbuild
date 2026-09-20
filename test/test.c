19e0fb17cb024a2465f5f65900e229087c6c8893
Qoqnus master <mahisokhary@gmail.com>
2026-09-20 18:01:22 +0330

    test: Set qbuild to debug mode

--- test/test.c
+++ test/test.c
@@ -1,8 +1,10 @@
 #include "test.h"
+#include <stdlib.h>
 
 extern bool test_qbuild();
 
 int main() {
+	setenv("QBUILD_DEBUG", "", true); // Set qbuild debug mode
 	bool success = 1;
 	success &= test_qbuild();
 	return !success;
