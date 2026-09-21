2bd1fef071263a0a1a65818aae1733439dfd16c7
Qoqnus master <mahisokhary@gmail.com>
2026-09-21 06:03:39 +0330

    qbuild: Added constant LOGGING_FAILED to typedef enum qbuild_result_t

--- qbuild/qbuild.h
+++ qbuild/qbuild.h
@@ -14,6 +14,7 @@ typedef enum {
 	QBUILD_RESULT_FILE_NOT_FOUND = 3, // Requested file not found
 	QBUILD_RESULT_CONTEXT_CREATE_FAILED = 4, // Failed to load resources needed for creating context
 	QBUILD_RESULT_DATASTRUCTURE_FAILED = 5, // Datastructure operation failed
+	QBUILD_RESULT_LOGGING_FAILED = 6, // Failed to log
 } qbuild_result_t;
 
 #ifdef __cplusplus
