7e04bc5b79b29ec48e70de177b40a5c485b55105
Qoqnus master <mahisokhary@gmail.com>
2026-09-24 06:07:16 +0330

    test/qbuild/string: Added test for function qbuild_string_format

--- test/qbuild/string.c
+++ test/qbuild/string.c
@@ -13,9 +13,25 @@ bool test_qbuild_string_lower() {
 	return success;
 }
 
+bool test_qbuild_string_format() {
+	test_run_log();
+	char *fmt = "my name is %s";
+	char *fmt_param = "qoqnus";
+	char *expected_res = "my name is qoqnus";
+	char *res;
+
+	bool success = true;
+	res = qbuild_string_format(fmt, fmt_param);
+	success &= strcmp(expected_res, res) == 0;
+	free(res);
+	test_result_log(success);
+	return success;
+}
+
 bool test_qbuild_string() {
 	bool success = true;
 	success &= test_qbuild_string_lower();
+	success &= test_qbuild_string_format();
 	return success;
 }
 
